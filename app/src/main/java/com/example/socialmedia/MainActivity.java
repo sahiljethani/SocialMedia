f77fe686f8b5dac6537d7baf71ab68de0e97a76e
Gunish Jain <gunish.jain@gmail.com>
2020-04-06 19:45:21 +0530

    Added Navbar with Header

--- app/src/main/java/com/example/socialmedia/MainActivity.java
+++ app/src/main/java/com/example/socialmedia/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.socialmedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationView navigationView;
+    private RecyclerView userpostList;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationView = (NavigationView)findViewById(R.id.navigationView);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                menuSelector(menuItem);
+                return false;
+            }
+        });
+
+    }
+
+    private void menuSelector(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
