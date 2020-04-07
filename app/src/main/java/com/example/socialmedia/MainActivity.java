a5120aa2f90d0f690fd67becf621ce2741d465d4
Gunish Jain <gunish.jain@gmail.com>
2020-04-07 13:58:44 +0530

    Added Click Functionality for Sidebar Navigation

--- app/src/main/java/com/example/socialmedia/MainActivity.java
+++ app/src/main/java/com/example/socialmedia/MainActivity.java
@@ -1,39 +1,57 @@
 package com.example.socialmedia;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SlidingDrawer;
 import android.widget.Toast;
-
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private RecyclerView userpostList;
     private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        navigationView = (NavigationView)findViewById(R.id.navigationView);
+
+        //Initialing Variables
+        toolbar = (Toolbar) findViewById(R.id.home_appbar_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
         View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
 
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        //Initializing Layout
+        setupActionBar();
+        setupDrawerLayout();
+        setupNavigationHeader();
+    }
 
-                menuSelector(menuItem);
-                return false;
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private void menuSelector(MenuItem menuItem) {
@@ -49,4 +67,26 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+    private void setupActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home");
+    }
+
+    private void setupDrawerLayout() {
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupNavigationHeader() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuSelector(menuItem);
+                return false;
+            }
+        });
+    }
+
 }
